ce9b432542f7f0a7a3eb8162a9b16e2dac885f48
Seba Di Lauro <jsdilauro@gmail.com>
2024-03-07 13:34:54 -0300

    add input to test

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,26 +1,53 @@
 import { Color4 } from '@dcl/sdk/math'
-import ReactEcs, { ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
+import ReactEcs, { Button, Input, Label, ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
 import { canvasInfo } from "./index";
+
+const widthFactor:number = .5
+const heightFactor:number = .5
+let currentValue: string = ''
 
 const uiComponent = () => (
   <UiEntity
-    uiTransform = {{
-      width:'90%',
-      height: '90%',
-      margin: { top: '5%', left: '5%' }
+    uiTransform={{
+      height: canvasInfo.height * heightFactor,
+      width: canvasInfo.width * widthFactor,
+      margin: { top: canvasInfo.height * (1.0 - heightFactor) / 2, left: canvasInfo.width * (1.0 - widthFactor) / 2 },
+      flexDirection: 'column',
     }}
-    uiBackground={{ color: Color4.Red() }}
+    uiBackground={{
+      color: Color4.Gray(),
+    }}
   >
-    <UiEntity
-      uiTransform = {{
-        width: canvasInfo.width * 0.8,
-        height: canvasInfo.height * 0.8,
-        margin: { top: canvasInfo.height * 0.05, left: canvasInfo.width * 0.05 }
+    <Input
+      onSubmit={(value) => {
+        handleSubmitText(value)
       }}
-      uiBackground={{ color: Color4.Blue() }}
+      fontSize={35}
+      placeholder={'type something'}
+      placeholderColor={Color4.Black()}
+      value={currentValue}
+      onChange={($) => (currentValue = $)}
+      uiTransform={{
+        height: '80px',
+        margin: '15px',
+      }}
+    ></Input>
+    <Button
+      value="Submit text"
+      variant="primary"
+      uiTransform={{ alignSelf: 'center', padding: '25px' }}
+      onMouseDown={() => {
+        handleSubmitText(currentValue)
+        currentValue = ''
+      }}
     />
   </UiEntity>
 )
+
+function handleSubmitText(value: string) {
+  console.log('submitted value: ' + value)
+  // do something with text
+}
 
 export function setupUi() {
   ReactEcsRenderer.setUiRenderer(uiComponent)
