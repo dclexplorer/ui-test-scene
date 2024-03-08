986edc3359f462ff12dd833b824e40014fdb5956
Seba Di Lauro <jsdilauro@gmail.com>
2024-03-08 15:43:00 -0300

    add ui-transform properties to test behavior

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,39 +1,60 @@
 import { Color4 } from '@dcl/sdk/math'
-import ReactEcs, { Button, Input, Label, ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
+import ReactEcs, { Button, ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
 import { canvasInfo } from "./index";
 
-const widthFactor:number = .1
-const heightFactor:number = .1
-let currentValue: string = ''
+
+const widthFactor:number = .3
+const heightFactor:number = .3
+var isMenuVisible: boolean = false
 
 const uiComponent = () => (
+  // parent
   <UiEntity
     uiTransform={{
-      height: canvasInfo.height * heightFactor,
+      minHeight: canvasInfo.height * heightFactor,
       width: canvasInfo.width * widthFactor,
       margin: { top: canvasInfo.height * (1.0 - heightFactor) / 2, left: canvasInfo.width * (1.0 - widthFactor) / 2 },
+      display: 'flex',
       flexDirection: 'column',
-      justifyContent: 'center'
+      justifyContent: 'flex-end',
+      alignItems: 'center' 
     }}
-    uiBackground={{
-      color: Color4.create(0, 0, 0, 0.8),
-    }}
+    uiBackground={{ color: Color4.Blue() }}
+
   >
-    <Button
-      value="Large text button with padding = 10px"
-      variant="primary"
-      uiTransform={{ alignSelf: 'center', padding:'50px'}}
-      onMouseDown={() => {
-        handleSubmitText(currentValue)
-        currentValue = ''
-      }}
-    />
+     
+      // Menu
+     <UiEntity
+      uiTransform={{
+          height: 100,
+          width: '100%',
+          alignContent: 'center',
+          justifyContent: 'center',
+          display: isMenuVisible ? 'flex': 'none'
+       }}
+        uiText={{
+          value: "Menu",
+          fontSize: 30
+       }}
+       uiBackground={{ color: Color4.Green() }}
+     />
+     // button
+     <Button
+       uiTransform={{
+          width: 100,
+          height: 30,
+         
+       }}
+       value="Toggle Menu"
+       uiBackground={{ color: Color4.Red() }}
+       onMouseUp = {toggleMenuVisibility}
+     />
   </UiEntity>
 )
 
-function handleSubmitText(value: string) {
-  console.log('submitted value: ' + value)
-  // do something with text
+// Function to toggle the state of the menu
+function toggleMenuVisibility(){
+ isMenuVisible = !isMenuVisible
 }
 
 export function setupUi() {
